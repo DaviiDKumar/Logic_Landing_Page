07ee3237fbd1236e0cec765e7b6bab8fa3912228
DaviDChoudharY <122169398+DaviDChoudharY@users.noreply.github.com>
2026-02-12 15:12:33 +0530

    update: implement meta-safe layout and compliance changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Main2 from './pages/Main2';
 import Main3 from './pages/Main3';
 import Main11 from './pages/Main11';
 import Main22 from './pages/Main22';
+import Leads from './pages/Leads';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/logic-page/secure/access-point" element={<Main2 />} />
         <Route path="/logic-page/secure/access-point-2" element={<Main22 />} />
         <Route path="/logic-page/terminal/live-signals" element={<Main3 />} />
+        <Route path="/logic-page/terminal/secure/isValid/Leads" element={<Leads />} />
       </Routes>
     </Router>
   );
